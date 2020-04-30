85375375913e9e38a6e68f73f6418469249cb822
Clark <clarkjzhang@gmail.com>
2020-04-30 19:49:42 -0400

    added value network

--- alpha_tictactoe/main.py
+++ alpha_tictactoe/main.py
@@ -9,6 +9,20 @@ from game_base import GameBase, run_game
 from neural_nets import ValueNetwork
 
 
+# gpu setup copied from https://www.tensorflow.org/guide/gpu
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
+
 if __name__ == "__main__":
     value_network = ValueNetwork()
     ttt_mcts = TicTacToe()
@@ -20,13 +34,18 @@ if __name__ == "__main__":
 
     ttt = TicTacToe()
 
-    for i in range(10):
+    optimizer = tf.keras.optimizers.Adam()
+
+    state_list = []
+    value_list = []
+
+    for i in range(100):
         if i % 2 == 0:
             data_dict = run_game(ttt, optimal_actor, mcts_actor)
         else:
             data_dict = run_game(ttt, mcts_actor, optimal_actor)
 
-        result = data_dict["result"]
+        result = data_dict["game_status"]
         if result == GameBase.Status.PLAYER1_WIN:
             value = 1
         elif result == GameBase.Status.PLAYER2_WIN:
@@ -34,11 +53,25 @@ if __name__ == "__main__":
         else:
             value = 0
 
-        states = np.array(data_dict["states"], dtype=np.float32).reshape((-1, 3, 3, 1))
-        
-        
 
+        state_list.extend([x[:9].reshape(3, 3, 1) for x in data_dict["states"]])
+        value_list.extend([value for _ in data_dict["states"]])
 
-    
+        if i > 5:
+            batch_size = 32
+            rand_idx = np.random.randint(len(state_list), size=(batch_size,))
+            x = [state_list[i] for i in rand_idx]
+            x = np.array(x, dtype=np.float32)
+            y = [value_list[i] for i in rand_idx]
+            y = np.array(y, dtype=np.float32).reshape((-1, 1))
+
+            with tf.GradientTape() as tape:
+                values = value_network(x)
+                loss = tf.reduce_mean(tf.square(values - y))
+            grads = tape.gradient(loss, value_network.trainable_variables)
+            optimizer.apply_gradients(zip(grads, value_network.trainable_variables))
+            print("{} -- Loss: {}".format(i, loss))
+
+    value_network.save_weights("models/model.ckpt")
 
 
--- alpha_tictactoe/test.py
+++ alpha_tictactoe/test.py
@@ -1,35 +1,46 @@
+import tensorflow as tf
 import numpy as np
 from tqdm import tqdm
 
 from minmax import OptimalActor
-from mcts import MCTS
+from mcts import MCTSActor
 from tictactoe import TicTacToe
-from game_base import GameBase
+from game_base import GameBase, run_game
+from neural_nets import ValueNetwork
 
-def run_game(mcts, minmax, game, mcts_player, num_expansions):
-    game.reset()
-    while game.get_game_status() == GameBase.Status.IN_PROGRESS:
-        curr_player = game.get_curr_player()
-        state = game.get_state()
-        if (curr_player == mcts_player):
-            action = mcts.search(state, mcts_player, num_expansions=num_expansions)
-        else:
-            action = minmax.get_action(state)
-        game.step(action)
 
-    outcome = game.get_outcome(mcts_player)
-    return outcome
+# gpu setup copied from https://www.tensorflow.org/guide/gpu
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
 
 if __name__ == "__main__":
-    num_runs = 100
-    num_expansions = 1000
+    num_runs = 1000
+    num_expansions = 100
 
     ttt = TicTacToe()
 
+
+    value_network = ValueNetwork()
+    value_network.build((1, 3, 3, 1))
+    value_network.load_weights("models/model.ckpt")
+
     ttt_mcts = TicTacToe()
-    mcts = MCTS(ttt_mcts)
+    mcts_actor = MCTSActor(ttt_mcts, num_expansions=num_expansions, value_network=value_network)
+    # mcts_actor = MCTSActor(ttt_mcts, num_expansions=num_expansions)
 
     optimal_data = np.load("data/minmax_cache.npy")
+    optimal_actor = OptimalActor(optimal_data)
+
 
     data_dict = dict()
     data_dict[GameBase.Player.PLAYER1] = []
@@ -37,42 +48,37 @@ if __name__ == "__main__":
     for i in tqdm(range(num_runs)):
         if i % 2 == 0:
             mcts_player = GameBase.Player.PLAYER1
-            minmax_player = GameBase.Player.PLAYER2
+            actor1 = mcts_actor
+            actor2 = optimal_actor
         else:
             mcts_player = GameBase.Player.PLAYER2
-            minmax_player = GameBase.Player.PLAYER1
+            actor1 = optimal_actor
+            actor2 = mcts_actor
+        result = run_game(ttt, actor1, actor2)
 
-        minmax = OptimalActor(minmax_player, optimal_data)
+        data_dict[mcts_player].append(result["game_status"])
 
-        result = run_game(mcts, minmax, ttt, mcts_player, num_expansions)
-        data_dict[mcts_player].append(result)
-
-
-    all_results = data_dict[GameBase.Player.PLAYER1] + data_dict[GameBase.Player.PLAYER2]
-    all_results = np.array(all_results)
-    all_wins = np.sum(all_results == 1)
-    all_losses = np.sum(all_results == -1)
-    all_ties = np.sum(all_results == 0)
 
     data_dict[GameBase.Player.PLAYER1] = np.array(data_dict[GameBase.Player.PLAYER1])
-    player1_wins = np.sum(data_dict[GameBase.Player.PLAYER1] == 1)
-    player1_losses = np.sum(data_dict[GameBase.Player.PLAYER1] == -1)
-    player1_ties = np.sum(data_dict[GameBase.Player.PLAYER1] == 0)
-
+    player1_wins = np.sum(data_dict[GameBase.Player.PLAYER1] == GameBase.Status.PLAYER1_WIN)
+    player1_losses = np.sum(data_dict[GameBase.Player.PLAYER1] == GameBase.Status.PLAYER2_WIN)
+    player1_ties = np.sum(data_dict[GameBase.Player.PLAYER1] == GameBase.Status.TIE)
 
     data_dict[GameBase.Player.PLAYER2] = np.array(data_dict[GameBase.Player.PLAYER2])
-    player2_wins = np.sum(data_dict[GameBase.Player.PLAYER2] == 1)
-    player2_losses = np.sum(data_dict[GameBase.Player.PLAYER2] == -1)
-    player2_ties = np.sum(data_dict[GameBase.Player.PLAYER2] == 0)
+    player2_wins = np.sum(data_dict[GameBase.Player.PLAYER2] == GameBase.Status.PLAYER2_WIN)
+    player2_losses = np.sum(data_dict[GameBase.Player.PLAYER2] == GameBase.Status.PLAYER1_WIN)
+    player2_ties = np.sum(data_dict[GameBase.Player.PLAYER2] == GameBase.Status.TIE)
 
+    all_wins = player1_wins + player2_wins
+    all_losses = player1_losses + player2_losses
+    all_ties = player1_ties + player2_ties
 
     print("Total Statistics\n============")
     print("wins: {}, losses: {}, ties: {}".format(all_wins, all_losses, all_ties))
     print("wins %: {}, losses %: {}, ties %: {}".format(
-        all_wins / len(all_results), 
-        all_losses / len(all_results), 
-        all_ties / len(all_results)))
-
+        all_wins / num_runs, 
+        all_losses / num_runs, 
+        all_ties / num_runs))
 
     print("\nPlayer 1 Statistics\n============")
     print("wins: {}, losses: {}, ties: {}".format(player1_wins, player1_losses, player1_ties))
